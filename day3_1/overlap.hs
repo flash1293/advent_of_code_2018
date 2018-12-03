3a37deee31c486e2673740ec60d034182983812c
Johannes Reuter <jreuter@inovex.de>
2018-12-03 11:09:27 +0100

    day 3

---
+++ day3_1/overlap.hs
@@ -0,0 +1,64 @@
+import           Data.List       (maximumBy, tails)
+import           Data.List.Split (splitOn)
+import           Data.Map        (alter, empty, toList)
+import           System.IO       (isEOF)
+
+main :: IO ()
+main = do
+  res <- readAllLines calculate
+  print res
+
+readAllLines :: ([String] -> a) -> IO a
+readAllLines cb = _readAllLines cb []
+
+_readAllLines :: ([String] -> a) -> [String] -> IO a
+_readAllLines cb currentLines = do
+  done <- isEOF
+  if done
+    then return (cb $ reverse currentLines)
+    else do
+      newLine <- getLine
+      _readAllLines cb (newLine : currentLines)
+
+data Claim = Claim
+  { uid    :: Int
+  , x      :: Int
+  , y      :: Int
+  , width  :: Int
+  , height :: Int
+  } deriving (Show)
+
+-- calculate :: [String] -> String
+calculate strs =
+  length . filter (== (-1)) $
+  map snd $ toList $ foldl setRangeForClaim empty claims
+  where
+    claims = map parseClaim strs
+    setRangeForClaim fabric Claim { uid = currentId
+                                  , width = w
+                                  , height = h
+                                  , x = x
+                                  , y = y
+                                  } =
+      foldl
+        (flip (alter (updateField currentId)))
+        fabric
+        [(x, y) | x <- [x .. (x + w - 1)], y <- [y .. (y + h - 1)]]
+    updateField uid Nothing = Just uid
+    updateField _ (Just _)  = Just (-1)
+
+parseClaim :: String -> Claim
+parseClaim = constructClaim . splitOn " "
+  where
+    constructClaim [(_:uid), _, coord, size] =
+      Claim
+      { uid = read uid
+      , x = getX coord
+      , y = getY coord
+      , width = getWidth size
+      , height = getHeight size
+      }
+    getX = read . (!! 0) . splitOn ","
+    getY = read . (!! 1) . splitOn "," . init
+    getWidth = read . (!! 0) . splitOn "x"
+    getHeight = read . (!! 1) . splitOn "x"
